676c46c90d6814942c4332253ca04e8a7e292c06
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-03 08:47:54 -0300

    feat: calculator page - exporta componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
 import Header from './Header';
 import Title from './Title';
+import SubTitle from './SubTitle';
+import WallAreas from './WallAreas';
+import DoorsOrWindows from './DoorsOrWindows';
+import FormCalculator from './FormCalculator';
 
 export {
   Header,
   Title,
+  SubTitle,
+  WallAreas,
+  DoorsOrWindows,
+  FormCalculator,
 };
