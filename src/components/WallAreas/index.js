de5dd8b732fde4607e8599529d90ac42348f6fa9
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-03 08:45:11 -0300

    feat: calculator page - cria componente WallAreas

---
+++ src/components/WallAreas/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SubTitle from '../SubTitle';
+import WallArea from '../WallArea';
+
+const WallAreas = () => (
+  <div>
+    <SubTitle subtitle="Informe a área das quatro paredes" />
+    <WallArea />
+    <WallArea />
+    <WallArea />
+    <WallArea />
+  </div>
+);
+
+export default WallAreas;
