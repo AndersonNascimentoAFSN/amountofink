56abb2fc05ea08a3bebba956950da823aa1ab0dc
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-05 10:16:08 -0300

    refactor: remove importação do componente DoorsOrWindows

--- src/components/FormCalculator/index.js
+++ src/components/FormCalculator/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import WallAreas from '../WallAreas';
-import DoorsOrWindows from '../DoorsOrWindows';
 
 const FormCalculator = () => {
   const handleSubmit = () => {
@@ -9,7 +8,6 @@ const FormCalculator = () => {
   return (
     <form onSubmit={ handleSubmit }>
       <WallAreas />
-      <DoorsOrWindows />
       <button type="submit">Calcular</button>
     </form>
   );
