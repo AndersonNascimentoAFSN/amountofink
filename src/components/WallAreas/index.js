823b37629c6b2a7d28c62fc9423cd540c0102a7c
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-05 10:18:09 -0300

    refactor: adiciona a prop wall para obter uma forma de identificar as respectivas paredes

--- src/components/WallAreas/index.js
+++ src/components/WallAreas/index.js
@@ -5,11 +5,10 @@ import WallArea from '../WallArea';
 const WallAreas = () => (
   <div>
     <SubTitle subtitle="Informe a área das quatro paredes" />
-    <WallArea />
-    <WallArea />
-    <WallArea />
-    <WallArea />
+    <WallArea wall="wall1" />
+    <WallArea wall="wall2" />
+    <WallArea wall="wall3" />
+    <WallArea wall="wall4" />
   </div>
 );
-
 export default WallAreas;
