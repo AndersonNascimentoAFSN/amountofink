c134bdce926eb2fddcd76319aee3bc534b12d8f8
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-10 10:13:02 -0300

    feat: css-styles - importação css básico

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Title = ({ title }) => (
-  <h1>
+  <h1 className="c-title">
     { title }
   </h1>
 );
