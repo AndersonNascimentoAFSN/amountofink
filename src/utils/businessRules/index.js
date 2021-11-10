a82595a727906f95f8c4cb4545a4bf2cf2fa355a
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-09 21:05:51 -0300

    feat: business-rules - exporta funções das regras de negócios

---
+++ src/utils/businessRules/index.js
@@ -0,0 +1,23 @@
+import minMaxWall from './minMaxWall';
+import minMaxWalls from './minMaxWalls';
+import wallsBiggerThanDoor from './wallsBiggerThanDoor';
+import wallsBiggerThanWindow from './wallsBiggerThanWindow';
+import calcAreaWalls from './calcAreaWalls';
+import calcAreaTotal from './calcAreaTotal';
+import validations from './validations';
+import qtyTotalOfInk from './qtyTotalOfInk';
+import sizeTinCalc from './sizeTinCalc';
+import sizesTin from './sizesTin';
+
+export {
+  minMaxWall,
+  minMaxWalls,
+  wallsBiggerThanDoor,
+  wallsBiggerThanWindow,
+  calcAreaWalls,
+  calcAreaTotal,
+  validations,
+  qtyTotalOfInk,
+  sizeTinCalc,
+  sizesTin,
+};
---
+++ src/utils/index.js
@@ -0,0 +1,24 @@
+import {
+  minMaxWall,
+  minMaxWalls,
+  wallsBiggerThanDoor,
+  wallsBiggerThanWindow,
+  calcAreaWalls,
+  calcAreaTotal,
+  validations,
+  qtyTotalOfInk,
+  sizeTinCalc,
+  sizesTin } from './businessRules';
+
+export {
+  minMaxWall,
+  minMaxWalls,
+  wallsBiggerThanDoor,
+  wallsBiggerThanWindow,
+  calcAreaWalls,
+  calcAreaTotal,
+  validations,
+  qtyTotalOfInk,
+  sizeTinCalc,
+  sizesTin,
+};
