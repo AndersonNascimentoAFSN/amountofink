40b981e0ccbd19325bd8c998db3c90aba35612ce
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-10 10:11:40 -0300

    feat: css-styles - importação css básico

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles.css';
 
 const Header = () => (
-  <header>
-    <h1>Amount of Ink</h1>
+  <header className="c-header">
+    <h1 className="c-header__title">Amount of Ink</h1>
   </header>
 );
 
