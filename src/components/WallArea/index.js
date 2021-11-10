f241347657088117ba76c9b853a8469dd124782b
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-09 21:24:05 -0300

    feat: refatora componente WallArea para utilizar o novo estado global

--- src/components/WallArea/index.js
+++ src/components/WallArea/index.js
@@ -1,36 +1,22 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { saveWallArea } from '../../actions/saveWallArea';
 
-const WallArea = ({ wall }) => {
+const WallArea = ({ dimensions, roomIndex, roomName }) => {
   const dispatch = useDispatch();
-  const [area, setArea] = useState();
-  const height = useRef('');
-  const width = useRef('');
-  const doors = useRef('');
-  const windows = useRef('');
+  const [dimensionsState, setDimensionsState] = useState(dimensions);
 
-  const handleWallArea = () => {
-    const valueHeight = height.current.value;
-    const valueWidth = width.current.value;
-    const valueDoors = doors.current.value;
-    const valueWindows = windows.current.value;
-
-    if (valueHeight && valueWidth) {
-      setArea({ [wall]:
-        { height: valueHeight,
-          width: valueWidth,
-          qtyDoor: valueDoors,
-          qtyWindow: valueWindows },
-      });
-    }
+  const handleWallArea = ({ target: { name, value } }) => {
+    setDimensionsState((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   useEffect(() => {
-    if (!height.current.value && !width.current.value) return;
-    dispatch(saveWallArea(area));
-  }, [dispatch, area]);
+    dispatch(saveWallArea({ roomIndex, roomName, dimensions: dimensionsState }));
+  }, [dispatch, dimensionsState, roomIndex, roomName]);
 
   return (
     <div>
@@ -38,29 +24,29 @@ const WallArea = ({ wall }) => {
         type="text"
         placeholder="Altura(m)"
         name="height"
-        ref={ height }
         onChange={ handleWallArea }
+        value={ dimensionsState.height }
       />
       <input
         type="text"
         placeholder="Largura(m)"
         name="width"
-        ref={ width }
         onChange={ handleWallArea }
+        value={ dimensionsState.width }
       />
       <input
         type="text"
         placeholder="quantidade de portas"
-        name="doors"
-        ref={ doors }
+        name="qtyDoor"
         onChange={ handleWallArea }
+        value={ dimensionsState.qtyDoor }
       />
       <input
         type="text"
         placeholder="quantidade de janelas"
-        name="windows"
-        ref={ windows }
+        name="qtyWindow"
         onChange={ handleWallArea }
+        value={ dimensionsState.qtyWindow }
       />
     </div>
   );
@@ -69,5 +55,12 @@ const WallArea = ({ wall }) => {
 export default WallArea;
 
 WallArea.propTypes = {
-  wall: PropTypes.string.isRequired,
+  dimensions: PropTypes.shape({
+    height: PropTypes.number,
+    width: PropTypes.number,
+    qtyDoor: PropTypes.number,
+    qtyWindow: PropTypes.number,
+  }).isRequired,
+  roomName: PropTypes.string.isRequired,
+  roomIndex: PropTypes.number.isRequired,
 };
