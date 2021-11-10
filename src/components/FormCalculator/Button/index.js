90545b655d32880173d140f1e4090909bbfbe926
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-10 10:18:33 -0300

    feat: css-styles - adiciona componente Button

---
+++ src/components/FormCalculator/Button/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Button = () => (
+  <button type="submit" className="calculator__form__button">Calcular</button>
+);
+
+export default Button;
